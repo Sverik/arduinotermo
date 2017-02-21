5f1d1f8239b48ccf9f93685c7bb6b8fe9f09de5a
Sven-Erik Petermann <sverik@gmail.com>
2017-02-22 00:24:09 +0200

    Initial

---
+++ src/main/java/com/po/iot/BrewmonApplication.java
@@ -0,0 +1,12 @@
+package com.po.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewmonApplication.class, args);
+	}
+}
