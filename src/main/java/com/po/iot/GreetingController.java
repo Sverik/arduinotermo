4aa09cc9ee1b10a59ec0c28152834a916304a9c0
Sven-Erik Petermann <sverik@gmail.com>
2017-02-22 01:03:39 +0200

    Vastus baasist

--- src/main/java/com/po/iot/GreetingController.java
+++ src/main/java/com/po/iot/GreetingController.java
@@ -1,14 +1,23 @@
 package com.po.iot;
 
+import static com.po.iot.brewdata.public_.tables.Bubbledata.BUBBLEDATA;
+
+import org.jooq.DSLContext;
+import org.jooq.Record1;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
+	@Autowired DSLContext dsl;
 
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return new Greeting(1, "kala " + name);
+		Record1<Integer> timestamp = dsl.select(BUBBLEDATA.TIMESTAMP).from(BUBBLEDATA).fetchAny();
+		int ts = timestamp.get(BUBBLEDATA.TIMESTAMP);
+		return new Greeting(ts, "kala " + name);
 	}
 }
